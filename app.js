729cc4784412c863ec9927682fe0ac1ec50ebedb
Muhammad Awais <muhammad.awais@itb.local>
2024-11-21 17:31:34 +0500

    these files are being committed to the repo

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3002;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World! This is my Node.js app deployed via GitHub Actions!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
